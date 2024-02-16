9b2f4bf8467619346bd7e382e0e30f839de21fed
Naveen Kumar <navin10102001@gmail.com>
2024-02-16 23:42:33 +0530

    created circular sector

--- components/reanimated.tsx
+++ components/reanimated.tsx
@@ -18,38 +18,49 @@ const CircularSector = forwardRef(
       radius,
       stroke,
       fill,
+      cx,
+      cy,
     }: {
       startAngle: number;
       endAngle: number;
       radius: number;
       stroke: string;
       fill: string;
+      cx: number;
+      cy: number;
     },
     ref
   ) => {
-    const d = describeArc(radius, startAngle, endAngle);
+    const d = describeArc(cx, cy, radius, startAngle, endAngle);
 
     return <Path ref={ref} d={d} stroke={stroke} fill={fill} />;
   }
 );
 
-const describeArc = (radius: number, startAngle: number, endAngle: number) => {
-  // Convert angles to radians
+const describeArc = (
+  centerX: number,
+  centerY: number,
+  radius: number,
+  startAngle: number,
+  endAngle: number
+) => {
   const startRadians = startAngle * (Math.PI / 180);
   const endRadians = endAngle * (Math.PI / 180);
 
-  // Calculate coordinates for start and end points
-  const startX = radius * Math.cos(startRadians);
-  const startY = radius * Math.sin(startRadians);
-  const endX = radius * Math.cos(endRadians);
-  const endY = radius * Math.sin(endRadians);
+  // Calculate initial coordinates (adjust for center origin)
+  const startX = centerX + radius * Math.cos(startRadians);
+  const startY = centerY + radius * Math.sin(startRadians);
+
+  // Calculate ending coordinates (adjust for center origin)
+  const endX = centerX + radius * Math.cos(endRadians);
+  const endY = centerY + radius * Math.sin(endRadians);
 
   // Construct the SVG path string
   const largeArcFlag = Math.abs(endRadians - startRadians) > Math.PI ? 1 : 0;
-  const d = `M ${radius},${radius}
+  const d = `M ${centerX},${centerY}
           L ${startX},${startY}
           A ${radius},${radius} 0 ${largeArcFlag} 0 ${endX},${endY}
-          L ${radius},${radius}
+          L ${centerX},${centerY}
           Z`;
 
   return d;
@@ -60,15 +71,15 @@ const AnimatedCircle = Animated.createAnimatedComponent(CircularSector);
 const ExamplesReanimated: React.FC = () => {
   const progress = useSharedValue(0);
 
-  console.log(progress.value);
-
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       {/* <Animated.View style={[{ width: width, height: 100, backgroundColor: "violet" }, animatedStyles]} /> */}
       <Svg height={width} width={width} viewBox={`0 0 ${width} ${width}`}>
         <AnimatedCircle
-          startAngle={0}
-          endAngle={progress.value}
+          cx={width / 2}
+          cy={width / 2}
+          startAngle={270}
+          endAngle={0}
           radius={width / 2}
           stroke="#DEAD"
           fill="#DEADED"
